9e44c7391f7af9622b41f4873e9cd84edc05bf73
omikeflies <omikeflies@gmail.com>
2022-10-09 16:43:09 +0200

    Estilo al background de la App

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ export function App() {
   return (
     <div className="App">
       <div className="card_body">
-        <div className="card_title">
-        Budget APP
+        <div className="card_title">BUDGET APP</div>
+        <div className="card_secondtitle">
+          {" "}
+          With this app you can calculate your Digital Marketing budget
+           (websitecreation, SEO and SEM){" "}
         </div>
-        <div className="card_secondtitle"></div>
-          With this app you can calculate your Digital Marketing budget (website
-          creation, SEO and SEM)        
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in
+        <p className="card_p">
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vivamus in
           lorem eu ante feugiat rhoncus. Suspendisse in ligula urna. Aliquam
           vulputate orci lacus, in pulvinar purus fermentum quis. In sed velit
-          at sem molestie luctus quis egestas nisl.</p>
-         <button className="card_button"> Calculate Budget</button>
-        
+          at sem molestie luctus quis egestas nisl.Donec molestie massa sit amet
+          lacus eleifend, sit amet varius felis ullamcorper. Morbi urna ante,
+          condimentum at est et, rhoncus feugiat metus.
+        </p>
+        <button className="card_button"> Calculate Budget</button>
       </div>
     </div>
   );
